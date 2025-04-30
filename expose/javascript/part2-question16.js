440ad02eb6658695368fef51e5275b111af5df93
Drowsyyyyy <146775161+Drowsyyyyy@users.noreply.github.com>
2025-04-29 23:04:54 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,1 +1,11 @@
+// part2-question16.js 
+
+let statistics = { redCars: 21, blueCars: 45, greenCars: 12, raceCars: 5, blackCars: 40, rareCars: 2 };
+
+for (const prop in statistics) { 
+  const value = statistics[prop]; 
+  if (prop.startsWith('r') || value % 2 !== 0) { 
+    console.log(value); 
+  } 
+}
 
